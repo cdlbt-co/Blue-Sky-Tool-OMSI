9a378b4805e77c0fc1655e7560e1b567910e00ef
swinghin <swinghin@hotmail.com>
2025-11-25 00:45:12 +0000

    Proper shutdown after mainwindow closes

--- Blue Sky/App.xaml.cs
+++ Blue Sky/App.xaml.cs
@@ -7,5 +7,9 @@ namespace Blue_Sky
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            this.ShutdownMode = ShutdownMode.OnMainWindowClose;
+        }
     }
 }
